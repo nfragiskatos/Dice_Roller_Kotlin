ed51456e686a07c7382669b519ca56c8791362ab
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2019-09-10 20:53:36 -0700

    hooked up button to click listener and changed the roll value to random number between 1 & 6

--- app/src/main/java/com/create/nfraggle/diceroller/MainActivity.kt
+++ app/src/main/java/com/create/nfraggle/diceroller/MainActivity.kt
@@ -2,11 +2,27 @@ package com.create.nfraggle.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        findViewById<Button>(R.layout.activity_main.)
+        val rollBtn: Button = findViewById(R.id.roll_btn)
+        rollBtn.text = "Let's Roll"
+        rollBtn.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        val randomInt = Random.nextInt(6) + 1
+        resultText.text = randomInt.toString()
     }
 }
