2c9f3e8436b9246208d4c52e464b8f945f9f9a2f
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2019-09-10 21:05:34 -0700

    Imported dice images to drawable resource for each value of a dice roll.
     Replaced the TextView with an ImageView
    
     Changed on click listener to set the resource of the ImageView to the appropriate dice image depending on the roll.

--- app/src/main/java/com/create/nfraggle/diceroller/MainActivity.kt
+++ app/src/main/java/com/create/nfraggle/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.create.nfraggle.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import kotlin.random.Random
@@ -21,8 +22,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.result_text)
         val randomInt = Random.nextInt(6) + 1
-        resultText.text = randomInt.toString()
+        val diceImg: ImageView = findViewById(R.id.dice_img)
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImg.setImageResource(drawableResource)
     }
 }
